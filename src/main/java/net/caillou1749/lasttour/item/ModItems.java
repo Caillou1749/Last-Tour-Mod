eda9dcbcd9e9eb9be3cb17e7029d6bab166c7082
Pierre ALLEGRE <pierre.allegre@limayrac.fr>
2025-04-29 11:49:35 +0200

    little update

--- src/main/java/net/caillou1749/lasttour/item/ModItems.java
+++ src/main/java/net/caillou1749/lasttour/item/ModItems.java
@@ -11,11 +11,6 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, LastTourMod.MOD_ID);
 
-    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
-            () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
-            () -> new Item(new Item.Properties()));
-
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
